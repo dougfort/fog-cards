692999a7e40ca26760c31b0fa864f7c991471fc1
dougfort <doug.fort@gmail.com>
2016-12-13 20:59:38 -0500

    crude implementation of 'move'
    seems to have some one-off errors

--- cards.hs
+++ cards.hs
@@ -1,9 +1,9 @@
 {-
 
 -}
-import           Control.Monad
+import           Control.Monad ()
 import           Data.Foldable as D
-import           Data.List
+import           Data.List     ()
 import           Data.Maybe
 import qualified Data.Sequence as SEQ
 import           Text.Printf
@@ -13,10 +13,10 @@ import           Shuffle
 data Suit = Clubs | Diamonds | Hearts | Spades
 
 instance Show Suit where
-  show Clubs = "C"
+  show Clubs    = "C"
   show Diamonds = "D"
-  show Hearts = "H"
-  show Spades = "S"
+  show Hearts   = "H"
+  show Spades   = "S"
 
 data Rank = Ace
           | R2
@@ -31,26 +31,44 @@ data Rank = Ace
           | Jack
           | Queen
           | King
+  deriving (Eq)
 
 instance Show Rank where
-  show Ace = "A"
-  show R2 = "2"
-  show R3 = "3"
-  show R4 = "4"
-  show R5 = "5"
-  show R6 = "6"
-  show R7 = "7"
-  show R8 = "8"
-  show R9 = "9"
-  show R10 = "10"
-  show Jack = "J"
+  show Ace   = "A"
+  show R2    = "2"
+  show R3    = "3"
+  show R4    = "4"
+  show R5    = "5"
+  show R6    = "6"
+  show R7    = "7"
+  show R8    = "8"
+  show R9    = "9"
+  show R10   = "10"
+  show Jack  = "J"
   show Queen = "Q"
-  show King = "K"
+  show King  = "K"
 
 data Card = Card Suit Rank
 
 instance Show Card where
   show (Card s r) = show s ++ show r
+
+-- | successor: returns true of the second card is a successor to the first
+-- | does not require suits to match
+succ :: Card -> Card -> Bool
+succ (Card _ r1) (Card _ r2)  | r1 == R2 && r2 == Ace = True
+                              | r1 == R3 && r2 == R2 = True
+                              | r1 == R4 && r2 == R3 = True
+                              | r1 == R5 && r2 == R4 = True
+                              | r1 == R6 && r2 == R5 = True
+                              | r1 == R7 && r2 == R6 = True
+                              | r1 == R8 && r2 == R7 = True
+                              | r1 == R9 && r2 == R8 = True
+                              | r1 == R10 && r2 == R9 = True
+                              | r1 == Jack && r2 == R10 = True
+                              | r1 == Queen && r2 == Jack = True
+                              | r1 == King && r2 == Queen = True
+                              | otherwise = False
 
 type Deck = [Card]
 
@@ -91,6 +109,10 @@ data Move = Move { sourceStack :: Int
                  , destStack   :: Int
                  }
 
+instance Show Move where
+  show m = "(" ++ show (sourceStack m) ++ ", " ++ show (sourceIndex m) ++ ") -> " ++
+    show (destStack m)
+
 -- | main entry point
 main :: IO ()
 main =
@@ -113,10 +135,10 @@ playloop s t = do displayTableau t
                                 Right (s', t') ->
                                   playloop s' t'
                     "move":xs -> case move xs t of
-                                   Left m -> do
-                                     putStrLn m
+                                   Nothing -> do
+                                     putStrLn "invalid move"
                                      playloop s t
-                                   Right t' ->
+                                   Just t' ->
                                      playloop s t'
                     _ -> do
                       putStrLn ("unknown input: '" ++ line ++ "'")
@@ -159,8 +181,8 @@ showStackRow i s | i < visible s = "..."
 -- | Deal out a new Card to each stack
 deal :: SEQ.Seq Card -> Tableau -> Either String (SEQ.Seq Card, Tableau)
 deal cs t
-  | null cs = Left "empty deck"
-  | any (null . cards) t = Left "empty column"
+  | SEQ.null cs = Left "empty deck"
+  | any (SEQ.null . cards) t = Left "empty column"
   | otherwise =
     let w = SEQ.length t
         ss = SEQ.zip (SEQ.take w cs) t
@@ -168,15 +190,18 @@ deal cs t
         f (c, s) t' = t' SEQ.|> Stack {cards=cards s SEQ.|> c, visible=visible s}
     in Right (cs', foldr f SEQ.empty ss)
 
-move :: [String] -> Tableau -> Either String Tableau
-move xs t = case parseMove xs of
-              Nothing -> Left "unparsable move command"
-              Just mc -> Left "'move' not implemented"
+move :: [String] -> Tableau -> Maybe Tableau
+move xs t = do
+  mc <- parseMove xs
+  performMove t mc
 
 parseMove :: [String] -> Maybe Move
 parseMove xs
-    | length is == 3 =
-      Just Move{sourceStack = is !! 0, sourceIndex = is !! 1, destStack = is !!2}
+    | length xs == 3 =
+      Just Move{sourceStack = head is
+               , sourceIndex = is !! 1
+               , destStack = is !!2
+               }
     | otherwise =
       Nothing
   where
@@ -185,4 +210,36 @@ parseMove xs
 parseInt :: String -> Maybe Int
 parseInt s = case reads s :: [(Int, String)] of
                [(n, "")] -> Just n
-               _ -> Nothing
+               _         -> Nothing
+
+performMove :: Tableau -> Move -> Maybe Tableau
+performMove t mc = do
+  s <- t !? sourceStack mc
+  d <- t !? destStack mc
+  (s', m) <- cut s (sourceIndex mc)
+  d' <- paste d m
+  return (SEQ.update (sourceStack mc) s' $ SEQ.update (destStack mc) d' t)
+
+-- | this is defined in a later version of the Sequence package
+(!?) :: SEQ.Seq a -> Int -> Maybe a
+s !? i
+  | i <= 0 = Nothing
+  | i >= SEQ.length s = Nothing
+  | otherwise = Just (SEQ.index s i)
+
+-- | cut a Sequence of cards from the source source stack
+cut :: Stack -> Int -> Maybe (Stack, SEQ.Seq Card)
+cut s i
+  | i < visible s = Nothing
+  | i == visible s && visible s == 0 =
+    Just (Stack {cards=SEQ.empty, visible=0}, cards s)
+  | i == visible s =
+    let n = SEQ.length (cards s) - i in
+      Just (Stack {cards=SEQ.drop n (cards s), visible=visible s-1}, SEQ.take n (cards s))
+  | otherwise =
+    let n = SEQ.length (cards s) - i in
+      Just (Stack {cards=SEQ.drop n (cards s), visible=visible s}, SEQ.take n (cards s))
+
+-- | paste a Sequence at the end of a stack
+paste :: Stack -> SEQ.Seq Card -> Maybe Stack
+paste s d = Just Stack {cards=cards s SEQ.>< d, visible=visible s}
